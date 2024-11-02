00297cacf055077d2ca377174f99c03695553ea1
subahan ali <subahanislam523@gmail.com>
2024-11-03 00:26:14 +0600

    added to todo front end

---
+++ src/pages/Todo.jsx
@@ -0,0 +1,9 @@
+const Todo = () => {
+  return (
+    <div>
+      <h2>This is todo page</h2>
+    </div>
+  );
+};
+
+export default Todo;
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,21 @@
+import App from "@/App";
+import Login from "@/pages/Login";
+import Register from "@/pages/Register";
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+  },
+  {
+    path: "/register",
+    element: <Register></Register>,
+  },
+  {
+    path: "/login",
+    element: <Login></Login>,
+  },
+]);
+
+export default router;
